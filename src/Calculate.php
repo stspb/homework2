740587f5df7f4452e08fe3a4e69a20729bb51ad0
st <s.chirkin@mail1.pro>
2021-06-10 09:49:45 +0300

    Calculate class

--- src/Calculate.php
+++ src/Calculate.php
@@ -1,8 +1,20 @@
 <?php
+
 namespace Homework2;
 
 
 class Calculate
 {
+    public function execute($str)
+    {
+        $str = $this->removeSubstr($str);
+        var_dump($str);
+    }
 
+    public static function removeSubstr($str)
+    {
+        $pattern = ['/\n/'];
+        $replacement = "";
+        return preg_replace($pattern, $replacement, $str);
+    }
 }
